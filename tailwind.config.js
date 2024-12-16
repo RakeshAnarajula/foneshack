419d0ceca98fc75714f3b3c78dad859d95a76cbd
Rakesh Anarajula <rakeshanarajula305@gmail.com>
2024-12-16 14:31:03 +0530

    update changes here

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'], // Add Montserrat font
+      },
       animation: {
         'slide-left': 'slideLeft 2s ease-out forwards',
         'slide-right': 'slideRight 2s ease-out forwards',
@@ -23,3 +26,33 @@ export default {
   },
   plugins: [],
 };
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}", // Specify the files to scan for Tailwind CSS classes
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         montserrat: ['Montserrat', 'sans-serif'], // Add Montserrat font
+//       },
+//       animation: {
+//         'slide-left': 'slideLeft 2s ease-out forwards',
+//         'slide-right': 'slideRight 2s ease-out forwards',
+//       },
+//       keyframes: {
+//         slideLeft: {
+//           '0%': { transform: 'translateX(-100%)', opacity: '0' },
+//           '100%': { transform: 'translateX(0)', opacity: '1' },
+//         },
+//         slideRight: {
+//           '0%': { transform: 'translateX(100%)', opacity: '0' },
+//           '100%': { transform: 'translateX(0)', opacity: '1' },
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
